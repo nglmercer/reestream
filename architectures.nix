a92234b7072e0510ac418ea41a71f4e1f48568f8
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2026-01-12 18:10:30 -0400

    feat: generate docker images and bundle packages

---
+++ architectures.nix
@@ -0,0 +1,15 @@
+[
+  # Linux
+  { arch = "x86_64"; os = "linux"; target = "x86_64-unknown-linux-gnu"; formats = ["deb" "rpm" "tar.xz"]; }
+  { arch = "aarch64"; os = "linux"; target = "aarch64-unknown-linux-gnu"; formats = ["deb" "rpm" "tar.xz"]; }
+  { arch = "armv7l"; os = "linux"; target = "armv7-unknown-linux-gnueabihf"; formats = ["tar.xz"]; }
+  { arch = "armv6l"; os = "linux"; target = "arm-unknown-linux-gnueabihf"; formats = ["tar.xz"]; }
+  # { arch = "riscv32"; os = "linux"; target = "riscv32gc-unknown-linux-gnu"; formats = ["tar.xz"]; }
+  # { arch = "riscv64"; os = "linux"; target = "riscv64gc-unknown-linux-gnu"; formats = ["tar.xz"]; }
+  # MacOS
+  { arch = "x86_64"; os = "macos"; target = "x86_64-apple-darwin"; formats = ["tar.xz"]; }
+  { arch = "aarch64"; os = "macos"; target = "aarch64-apple-darwin"; formats = ["tar.xz"]; }
+  # Windows
+  { arch = "x86_64"; os = "windows"; target = "x86_64-pc-windows-msvc"; formats = ["zip"]; }
+  # { arch = "i686"; os = "windows"; target = "i686-pc-windows-msvc"; formats = ["zip"]; }
+]
--- flake.nix
+++ flake.nix
@@ -1,74 +1,203 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    crane.url = "github:ipetkov/crane";
-    fenix.url = "github:nix-community/fenix";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  } @ inputs: let
-      fenix = inputs.fenix.packages;
-    in
-    # Iterate over Arm, x86 for MacOs 🍎 and Linux 🐧
-    (flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        crane = inputs.crane.mkLib pkgs;
-        # Toolchain
-        toolchain = fenix.${system}.fromToolchainFile {
-          file = ./rust-toolchain.toml;
-          sha256 = "sha256-sqSWJDUxc+zaz1nBWMAJKTAGBuGWP25GCftIOlCEAtA=";
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (baseSystem:
+      let
+        cargoManifest = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        architectures = import ./architectures.nix;
+
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          system = baseSystem;
+          inherit overlays;
         };
-        craneLib = crane.overrideToolchain toolchain;
+        lib = pkgs.lib;
 
-        buildInputs = with pkgs; [
-          openssl.dev
-          pkg-config
-        ];
+        uniqueBy = filter: list:
+            let
+              aux = seen: remaining:
+                if remaining == [] then []
+                else
+                  let
+                    item = builtins.head remaining;
+                    rest = builtins.tail remaining;
+                  in
+                  if builtins.elem (filter item) seen then
+                    aux seen rest
+                  else
+                    [item] ++ aux (seen ++ [(filter item)]) rest;
+            in aux [] list;
 
-        src = pkgs.lib.cleanSourceWith {
-          src = craneLib.path ./.;
-          filter = path: type:
-            (pkgs.lib.hasInfix "/assets" path)
-            || (craneLib.filterCargoSources path type);
-        };
-        commonArgs = {
-          doCheck = false;
-          inherit src buildInputs;
-        };
-
-        libraries = with pkgs; [ openssl ];
-        # Compile all artifacts
-        appDeps = craneLib.buildDepsOnly commonArgs;
-
-        # Compile
-        app = craneLib.buildPackage (commonArgs // {
-          cargoArtifacts = appDeps;
-        });
-      in {
-        # nix build
-        packages.default = app;
-
-        # nix run
-        apps.default = flake-utils.lib.mkApp {
-          drv = app;
+        mkCrossPkgs = { arch, os }: let
+          cross = arch + "-" + os;
+          crossSystem = lib.systems.elaborate cross;
+        in import nixpkgs {
+          inherit overlays;
+          crossSystem = if cross != "x86_64-linux" then crossSystem else null;
+          localSystem = baseSystem;
         };
 
-        # nix develop
-        devShells.default = craneLib.devShell {
-          inherit buildInputs;
-
-          packages = with pkgs; [
-          openssl.dev
-            toolchain
-          ];
-
-          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}";
+        mkDevShell = { arch, os, ... }: pkgs.mkShell {
+          packages = with pkgs; [ rustc cargo ];
+          shellHook = ''
+            echo "DevShell for reestream ${arch}-${os}"
+          '';
         };
-      }
-    ));
+
+        mkPackage = { arch, os, target, ... }: let
+          crossPkgs = mkCrossPkgs { inherit arch os; };
+        in pkgs.rustPlatform.buildRustPackage {
+          name = cargoManifest.package.name;
+          tag = cargoManifest.package.version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          CARGO_BUILD_TARGET = target;
+          HOST_CC = lib.optionalString (os != "windows") "${pkgs.stdenv.cc.nativePrefix}cc";
+          TARGET_CC = lib.optionalString (os != "windows") "${crossPkgs.stdenv.cc.targetPrefix}cc";
+
+          buildInputs = with pkgs; [
+            openssl
+            pkg-config
+          ]
+            ++ lib.optionals (os == "linux") [ stdenv.cc  glibc ]
+            ++ lib.optionals (os == "macos") [ clang darwin.apple_sdk.frameworks.CoreFoundation ];
+        };
+
+        containerPkg = variant: let
+          pkg = mkPackage variant;
+          dockerPlatform =
+            if variant.arch == "x86_64" then "amd64"
+            else if variant.arch == "aarch64" then "arm64"
+            else if variant.arch == "armv7l" then "arm"
+            else if variant.arch == "armv6l" then "arm"
+            else if variant.arch == "i686" then "386"
+            else throw "Unsupported arch: ${variant.arch}";
+        in pkgs.dockerTools.buildLayeredImage rec {
+          name = cargoManifest.package.name;
+          tag = cargoManifest.package.version;
+          created = "now";
+          architecture = dockerPlatform;
+
+          contents = [ pkg ];
+          config.Cmd = ["/bin/${name}"];
+        };
+
+        generatedMatrixJson = builtins.toJSON (lib.flatten (map ({ arch, os, formats, ... }:
+          map (format: {
+            arch = arch;
+            os = os;
+            format = format;
+            package = "${os}-${arch}";
+            bundler = if format == "zip" then ".#toZip"
+              else if format == "deb" then "github:NixOS/bundlers#toDEB"
+              else if format == "rpm" then "github:NixOS/bundlers#toRPM"
+              else ".#toTarball";
+          }) formats
+        ) architectures));
+
+        archList = uniqueBy (i: i.arch) architectures;
+        generatedArchJson = builtins.toJSON archList;
+      in
+      {
+        devShells = lib.listToAttrs (map ({ arch, os, target, ... }: {
+          name = "${os}-${arch}";
+          value = mkDevShell { inherit arch os target; };
+        }) architectures) // {
+          # Default Devshell
+          default = mkDevShell {
+            arch = "x86_64";
+            os = "linux";
+            target = "x86_64-unknown-linux-gnu";
+          };
+        };
+
+        packages = (lib.listToAttrs (map ({ arch, os, target, ... }: {
+          name = "${os}-${arch}";
+          value = mkPackage { inherit arch os target; };
+        }) architectures)) // (pkgs.lib.listToAttrs (map ({arch, ...} @ args: {
+          name = "image-${arch}";
+          value = containerPkg args;
+        }) archList));
+
+        apps = {
+          help = {
+            type = "app";
+            program = toString (pkgs.writeScript "help" ''
+              #!/bin/sh
+              echo ""
+              echo "Welcome to ${cargoManifest.package.name}!"
+              echo ""
+              echo -e "\033[0;33mAvailable architectures:\033[0m"
+              ${lib.concatMapStringsSep "\n" (arch: ''echo "  - ${arch}"'') (lib.lists.unique (map ({ arch, ... }: arch) architectures))}
+              echo ""
+              echo -e "\033[0;35mAvailable OS:\033[0m"
+              ${lib.concatMapStringsSep "\n" (os: ''echo "  - ${os}"'') (lib.lists.unique (map ({ os, ... }: os) architectures))}
+              echo ""
+              echo -e "\033[0;32mTo build docker image, use:\033[0m"
+              echo "  nix build .#image-<arch>"
+              echo -e "\033[0;32mTo build a specific variant, use:\033[0m"
+              echo "  nix build .#<os>-<arch>"
+              echo ""
+              echo -e "\033[0;32mExample:\033[0m"
+              echo "  nix build .#linux-x86_64"
+            '');
+          };
+
+          matrix = {
+            type = "app";
+            program = toString (pkgs.writeScript "generate-matrix" ''
+              #!/bin/sh
+              echo '${generatedMatrixJson}'
+            '');
+          };
+
+          archs = {
+            type = "app";
+            program = toString (pkgs.writeScript "generate-arch-list" ''
+              #!/bin/sh
+              echo '${generatedArchJson}'
+            '');
+          };
+        };
+
+        bundlers = let
+          compress = ext: name: drv: pkgs.stdenv.mkDerivation {
+            name = "${name}-${drv.name}";
+            buildInputs = with pkgs; [ouch];
+            unpackPhase = "true";
+            buildPhase = ''
+              export HOME=$PWD
+              mkdir -p ./nix/store/
+              mkdir -p ./bin
+              for item in "$(cat ${pkgs.referencesByPopularity drv})"
+              do
+                cp -r $item ./nix/store/
+              done
+
+              cp -r ${drv}/bin/* ./bin/
+
+              chmod -R a+rwx ./nix
+              chmod -R a+rwx ./bin
+              ouch c nix bin ${drv.name}.${ext}
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp -r *.${ext} $out
+            '';
+          };
+        in {
+          toTarball = compress "tar.xz" "tar-simple";
+          toZip = compress "zip" "zip-simple";
+        };
+      });
 }
